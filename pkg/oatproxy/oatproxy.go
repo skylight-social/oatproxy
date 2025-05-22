be7fe357a2899c0bc80bdeed0aed97bec903337f
rcharmeyer <rcharmeyer@hotmail.com>
2025-05-22 10:14:21 -0700

    Preserve search params

--- pkg/oatproxy/oatproxy.go
+++ pkg/oatproxy/oatproxy.go
@@ -64,7 +64,11 @@ func New(conf *Config) *OATProxy {
 	o.Echo.GET("/oauth/upstream/jwks.json", o.HandleJwksUpstream)
 	o.Echo.GET("/oauth/downstream/client-metadata.json", o.HandleClientMetadataDownstream)
 	o.Echo.GET("/app-return/android", func(c echo.Context) error {
-		return c.Redirect(http.StatusTemporaryRedirect, "https://skylight.social/app-return/android")
+		redirectURL := "https://skylight.social/app-return/android"
+		if len(c.QueryParams()) > 0 {
+			redirectURL += "?" + c.Request().URL.RawQuery
+		}
+		return c.Redirect(http.StatusTemporaryRedirect, redirectURL)
 	})
 	o.Echo.Any("/xrpc/*", o.OAuthMiddleware(o.HandleWildcard))
 	o.Echo.Use(o.ErrorHandlingMiddleware)
